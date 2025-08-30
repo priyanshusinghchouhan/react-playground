f56f6c070da4f7bac08d382cba6f2a7c6b257b4f
Priyanshu Singh Chouhan <janneker9@gmail.com>
2025-08-30 08:05:41 +0530

    Initial Commit

---
+++ 08-basic-react-Recoil/src/code-challenge/05-Todos.js
@@ -0,0 +1,10 @@
+
+export const TODOS = [{
+    id: 1,
+    title: "Pedri Gonzalez",
+    description: "The best midfielder in the world"
+}, {
+    id: 2,
+    title: "Pau Cubarsi",
+    description: "The best young defender in the world"
+}]
